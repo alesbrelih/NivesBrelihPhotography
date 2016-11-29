2904caac6968baa97ac334d9580481f125cbf96d
alesbrelih <Ales@DESKTOP-P5T7629>
2016-11-29 19:44:59 +0100

    Implemented albums service.
    Still needs to be tested.

--- Scripts/Admin_Angular/services/AlbumsService/albums.service.js
+++ Scripts/Admin_Angular/services/AlbumsService/albums.service.js
@@ -6,16 +6,18 @@
     var app = angular.module("adminApp.services");
 
     //factory controller
-    function albumsFactoryController($http,toastr) {
+    function albumsFactoryController($http,toastr,$state) {
         
         //returned factory
         var albumsFactory = {};
 
         // --- privates --- //
-        var _albums = [];
+        var albums = [];
+
+
 
         //---- properties ----- //
-        albumsFactory.Albums = _albums;
+        albumsFactory.Albums = albums;
 
 
 
@@ -29,14 +31,14 @@
                 .then(function (success) {
 
                     //clear if refresh
-                    if (_albums.length > 0) {
-                        while (_albums.length>0) {
-                            _albums.pop();
+                    if (albums.length > 0) {
+                        while (albums.length>0) {
+                            albums.pop();
                         }
                     }
 
                 success.data.forEach(function(item) {
-                    _albums.push(item);
+                    albums.push(item);
                 });
             }, function(err) {
                     if (err) {
@@ -48,6 +50,57 @@
                 return q;
             }
         };
+
+        //get single album and returns promise for resolve edit
+        albumsFactory.GetAlbums = function(id) {
+            return $http.get("/api/albums", {
+                params: {
+                    "id": id
+                }
+            });
+        }
+
+        //creates album
+        albumsFactory.CreateAlbum = function (album) {
+
+            //post data to api
+            $http.post("/api/albums", album)
+                .then(function (success) {
+
+                    //on success show toastr, add album to albusm array and redirect to main albums page if needed
+                    toastr.success("Album successfully created", "Success");
+                    
+                    if ($state.current.name === "albums-add") {
+                        $state.go("albums");
+                    } else {
+                        albums.push(success.data);
+                    }
+
+                },
+                function (err) {
+                    //catch err and show it
+                    toastr.error(err.data, "Error");
+                    console.log(err.data);
+                });
+        }
+
+        //edits album
+        albumsFactory.EditAlbum = function (album) {
+
+            //call api with put request
+
+            $http.put("/api/albums", album)
+                .then(function () {
+                    //album successfully edited -> show toastr and change state to albums if on albums-edit
+                    toastr.success("Album successfully edited");
+                    if ($state.current.name === "albums-edit") {
+                        $state.go("albums");
+                    }
+                }, function (err) {
+                    //err catch
+                    toastr.error(err.data, "Error");
+                });
+        }
 
         //delete album
         albumsFactory.DeleteAlbum = function (album, deletePhotos) {
@@ -61,18 +114,22 @@
                 })
                 .then(function() {
                     toastr.success("Album successfully deleted");
-                    var albumIndex = _albums.indexOf(album);
-                    _albums.splice(albumIndex);
+                    var albumIndex = albums.indexOf(album);
+                    albums.splice(albumIndex);
                 }).catch(function (err) {
                 toastr.error(err.data, "Error");
                 console.log(err);
             });
         }
 
+
+        //return singleton
         return albumsFactory;
     }
 
-    albumsFactoryController.$inject = ["$http","toastr"];
+    albumsFactoryController.$inject = ["$http", "toastr", "$state"];
+
+
     //register factory on angular module
     app.factory("AlbumsService", albumsFactoryController);
 
