eebce2431157b590d8a7bee608989ce1e73c1399
alesbrelih <ales.brelih@gmail.com>
2017-02-22 16:52:52 +0100

    Updated visual on home carousel. On NBP logo. It shows text when hovered.

--- Scripts/WelcomeCarousel_Scripts/welcome-carousel.js
+++ Scripts/WelcomeCarousel_Scripts/welcome-carousel.js
@@ -4,4 +4,11 @@
 
 $(function () {
     $(".carousel-inner .item").first().addClass("active");
+
+    $(".continue").on("mouseenter", function() {
+        $(".enter-page-title").slideDown();
+    });
+    $(".continue").on("mouseleave", function() {
+        $(".enter-page-title").slideUp();
+    });
 });
