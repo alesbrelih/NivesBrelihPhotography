6896cad05d0ce2d9d6cc516f3fee17b97eced8b8
ales.brelih@gmail.com <ales.brelih@gmail.com>
2017-10-18 20:54:30 +0200

    added missing Error handler class for the err handling

---
+++ HelperClasses/ErrorHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace NivesBrelihPhotography.HelperClasses
+{
+    public enum ServerType
+    {
+        API,
+        SERVER
+    }
+    
+    public static class ErrorHandler
+    {
+        private static string apiErrorPath = Path.Combine(HttpContext.Current.Server.MapPath("~"),"Log/API/log.txt");
+        public static void ServerError(ServerType type, Exception ex )
+        {
+            string text = String.Format("{0} ---> {1}", DateTime.Now.ToString("yyyy-MM-dd - HH:mm:ss"), ex.Message);
+            if (type == ServerType.API)
+            {
+                using (TextWriter writer = new StreamWriter(apiErrorPath, true))
+                {
+                    writer.WriteLine(text);
+                }
+
+                
+            }
+        }
+    }
+}
