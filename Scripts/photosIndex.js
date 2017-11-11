db407c82776029ea0711173c749c26facd1461b7
ales.brelih@gmail.com <ales.brelih@gmail.com>
2017-11-11 20:53:06 +0100

    when category is selected on mobile, wrapper gets automatically hidden

--- Scripts/photosIndex.js
+++ Scripts/photosIndex.js
@@ -1,28 +1,39 @@
-﻿function SetMasonryPhotosIndex() {
-        $($("#photos").imagesLoaded(function () {
-            $('.masonry-layout').masonry({
-                // options
-                itemSelector: '.masonry-image',
-                columnWidth: '.grid-sizer'
-                //percentPosition: true
-            });
-        }));
-
-        //delegating hover to display discription even on dynamically added photos
-        $(".masonry-layout").on("mouseenter mouseleave", ".masonry-image", function () {
-            $(this).find(".img-description").toggleClass("show-description"); //shows description
+﻿var $categoryNavShowHide = false;
+function SetMasonryPhotosIndex(smallscreen) {
+    $($("#photos").imagesLoaded(function () {
+        $('.masonry-layout').masonry({
+            // options
+            itemSelector: '.masonry-image',
+            columnWidth: '.grid-sizer'
+            //percentPosition: true
         });
+    }));
 
+    //delegating hover to display discription even on dynamically added photos
+    $(".masonry-layout").on("mouseenter mouseleave", ".masonry-image", function () {
+        $(this).find(".img-description").toggleClass("show-description"); //shows description
+    });
+
+    if (smallscreen) {
+        var $wrapper = $(".portfolio-categories-container.dropdown-container .dropdown-wrapper");
+        if ($wrapper.hasClass("shown")) {
+            $wrapper.slideUp("fast", function () {
+                $wrapper.removeClass("shown");
+                $categoryNavShowHide = false;
+            })
+        }
     }
-/* PREVENT GLOBAL SCOPING*/
-(function(jQuery) {
 
-    
+}
+/* PREVENT GLOBAL SCOPING*/
+(function (jQuery) {
+
+
     var $ = jQuery;
 
     var ajaxLoadPictures = false;
 
-    var $categoryNavShowHide = false;
+
 
     var $loaderWrapper = $(".loader-wrapper-photos");
 
@@ -45,7 +56,7 @@
                 }
                 console.log($wrapper);
             }
-            
+
         });
     }
 
@@ -85,7 +96,7 @@
             ajaxLoadPictures = true;
 
             $loaderWrapper.fadeIn();
-            $.get("/Photos/LoadPhotos", { pageNumber: pageNm + 1 }, function(data) {
+            $.get("/Photos/LoadPhotos", { pageNumber: pageNm + 1 }, function (data) {
                 //checking if returned json is null and stopping function
                 if (jQuery.isEmptyObject(data)) {
 
@@ -97,15 +108,15 @@
                 }
                 //html to be inserted
                 var html = "";
-                $.each(data, function(i, item) {
+                $.each(data, function (i, item) {
                     var row = '<div class="masonry-image img-container col-xs-12 col-sm-6 col-md-4">' +
-                        '<div class="img-wrapper">'+
-                            '<a href="/Images/Photos/MID/'+item.PhotoUrl+'" data-lightbox="'+item.PhotoUrl+'">'+
-                                '<img alt="'+item.PhotoTitle+'" src="/Images/Photos/MIN/' + item.PhotoUrl + '" data-href="/Images/Photos/MID/'+item.PhotoUrl+'" class="img-responsive"/>' +
-                                '<div class="img-description"><span>' + item.PhotoTitle + '</span></div>' +
-                            '</a>' +
-                        '</div>'+
-                       '</div>';
+                        '<div class="img-wrapper">' +
+                        '<a href="/Images/Photos/MID/' + item.PhotoUrl + '" data-lightbox="' + item.PhotoUrl + '">' +
+                        '<img alt="' + item.PhotoTitle + '" src="/Images/Photos/MIN/' + item.PhotoUrl + '" data-href="/Images/Photos/MID/' + item.PhotoUrl + '" class="img-responsive"/>' +
+                        '<div class="img-description"><span>' + item.PhotoTitle + '</span></div>' +
+                        '</a>' +
+                        '</div>' +
+                        '</div>';
                     html += row;
                 });
                 //changing html to json so masonry appended can be called on
@@ -124,13 +135,13 @@
                     photoContainer.masonry('appended', jqueryHtml);
                     ajaxLoadPictures = false;
                     $loaderWrapper.fadeOut();
-                    
+
                 });
                 pageNm++;
             });
         }
 
-//when scroled to bottom trying to load more images if wanted to
+        //when scroled to bottom trying to load more images if wanted to
         $(window).scroll(function () {
 
             if ($(window).scrollTop() === 0) {
@@ -147,7 +158,7 @@
             }
         });
 
-        $("#category-selector").children().on("click", function() {
+        $("#category-selector").children().on("click", function () {
             //when clicked on random category page is reseted 
             //and the if that allows checking for new photos is reseted
             photosEnd = false;
@@ -165,7 +176,7 @@
         });
 
         //click on category fixed
-        $("#category-button").click(function() {
+        $("#category-button").click(function () {
             //slide the category selector div
             $("#category-navigation").slideToggle();
 
